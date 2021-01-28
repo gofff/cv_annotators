32ca4f0589b88f5892c29ce38c02341e0284e6a8
gofff <khatiullin@gmail.com>
2021-01-28 13:34:02 +0300

    Add KeyCode constants and custom types

---
+++ cv_annotators/constants.py
@@ -0,0 +1,8 @@
+
+
+class KeyCode:
+
+    ESC = 27
+    ENTER = 13
+    NEXT = ord(' ')
+    UNDO = ord('z')
---
+++ cv_annotators/custom_types.py
@@ -0,0 +1,4 @@
+from typing import Tuple, List
+
+Point_t = Tuple[int, int] # x, y
+Circle_t = Tuple[Point_t, float] # (x_coord, y_coord, radius)
